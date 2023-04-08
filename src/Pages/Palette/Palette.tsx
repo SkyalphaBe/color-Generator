55624c7153ee21969a6438362f84f58b22c2ca4e
François Graux <frsgra28@gmail.com>
2023-04-08 23:22:14 +0200

    menu palette + components Palletteline

--- src/Pages/Palette/Palette.tsx
+++ src/Pages/Palette/Palette.tsx
@@ -1,9 +1,20 @@
 import React from "react";
+import "./Palette.scss";
+import PaletteLine from "../../Components/PaletteComponents/PaletteLine";
 
 function Palette(){
+    const [contentState, setContentState] = React.useState<boolean>(true);
     return (
-        <div>
-            <h1>Palette</h1>
+        <div className={"palette"}>
+            <div className={"menu"}>
+                {contentState ?
+                    <div className={"content"}>
+                        <PaletteLine name={"palette 1"} nbColor={6} date={"08/04/23"}/>
+                    </div>
+                    :
+                    <h1>There is no color-palette for the moment :( </h1>}
+                <button className={"btnNew"}>+</button>
+            </div>
         </div>
     );
 }
