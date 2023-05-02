ebdff17940eacd4aa49001349bf7ae0be60e9b87
François Graux <frsgra28@gmail.com>
2023-05-02 21:53:21 +0200

    Added fetch API getPalettes

--- frontend/src/Components/PaletteComponents/PaletteLine/PaletteLine.tsx
+++ frontend/src/Components/PaletteComponents/PaletteLine/PaletteLine.tsx
@@ -1,12 +1,11 @@
 import React from "react";
 import "./PaletteLine.scss";
 
-function PaletteLine(props: {name: string,nbColor:number,date: string }) {
+function PaletteLine(props: {name: string,nbColor:number }) {
     return (
         <div className={"paletteLine"}>
             <div className={"paletteLine__name"}>Name : {props.name}</div>
             <div className={"paletteLine__nbColor"}>Nb color : {props.nbColor}</div>
-            <div className={"paletteLine__date"}>Date : {props.date}</div>
         </div>
     );
 }
--- frontend/src/Pages/Palette/Palette.tsx
+++ frontend/src/Pages/Palette/Palette.tsx
@@ -1,20 +1,39 @@
-import React from "react";
+import React, {useEffect} from "react";
 import "./Palette.scss";
 import PaletteLine from "../../Components/PaletteComponents/PaletteLine/PaletteLine";
 import PortalModal from "../../Components/PortalModal/PortalModal";
 
+type PaletteProps = {
+    name: string,
+    nbColor: number,
+}
+
 function Palette(){
     const [contentState, setContentState] = React.useState<boolean>(false);
+    const [palette, setPalette] = React.useState<Array<PaletteProps>>([]);
+    const fetchData = (url: RequestInfo | URL) => fetch(url).then(res => res.json());
+
+    useEffect(() => {
+        fetchData("http://localhost:8080/api/palettes")
+            .then(data => {
+                if (data.length > 0) {
+                    setContentState(true);
+                    setPalette(data);
+                }
+            }
+        );
+    }, []);
 
     return (
         <div className={"palette"}>
             <div className={"menu"}>
                 {contentState ?
                     <div className={"content"}>
-                        <PaletteLine name={"palette 1"} nbColor={6} date={"08/04/23"}/>
+                        {palette.map((palette: PaletteProps) => <PaletteLine name={palette.name} nbColor={0}/>)}
                     </div>
                     :
-                    <h1>There is no color-palette for the moment :( </h1>}
+                    <h1>There is no color-palette for the moment :( </h1>
+                }
                 <PortalModal/>
             </div>
         </div>
